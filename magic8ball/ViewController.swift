0b9db6feca6d55b0d025d26ce1ed2c456d6c1b91
Rob Ranf <robranf@gmail.com>
2020-02-23 16:00:51 -0800

    Initial Commit

---
+++ magic8ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  magic8ball
+//
+//  Created by Rob Ranf on 2/23/20.
+//  Copyright © 2020 Rob Ranf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
